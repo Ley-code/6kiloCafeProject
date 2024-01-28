a63b63f80286ab9b4ff128120d5397ac4645b262
Amir-Fasil <amirfasil09@gmail.com>
2024-01-28 22:48:31 +0300

    login notification

--- Main.java
+++ Main.java
@@ -26,6 +26,7 @@ public class Main {
         }
         // ---------------------------------------------------------------------------------------------------
         new LoginPage(); // launches the sign-in page
+        System.out.println("login successful");
     }
 
 }
