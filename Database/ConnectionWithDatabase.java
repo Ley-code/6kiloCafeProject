4b4b914a478bec65ca22aad5252bf735f7155ac2
Leykun Birhanu <leykunbi12@gmail.com>
2024-01-30 16:04:50 +0300

    updated the database from amir

--- Database/ConnectionWithDatabase.java
+++ Database/ConnectionWithDatabase.java
@@ -1,11 +1,14 @@
 package Database;
 
 import GUI.Admin.graphAndChartPage;
+import GUI.Student.LoginPageForStudent;
 
+import javax.swing.table.DefaultTableModel;
 import java.sql.*;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -194,27 +197,201 @@ public class ConnectionWithDatabase {
         }
         return studentInDepartment;
     }
-    /*public static boolean pollTableCreator(List<String> foods,String tableName){
+    // This only creates a poll table write the code that add values to the table after Esube finished the GUI
+    public static void  pollTableCreator(List<String> foods,String tableName){
         try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
             String sqlQueryF = "CREATE TABLE " + tableName + " (" +
-                    "Row Number INT AUTO_INCREMENT PRIMARY KEY, " +
-                    "student_ID VARCHAR(15) " +
-                    "FOREIGN KEY(student_ID) REFERENCES students (student_ID) ";
+                    "Number INT AUTO_INCREMENT PRIMARY KEY " +
+                    "Question VARCHAR(200) );"; // CHECK THIS wHEN YOU FINISH
             PreparedStatement creatingStat = connection.prepareStatement(sqlQueryF);
-            int resultF = creatingStat.executeUpdate();
-            
-
+            System.out.println("is it here");
+            creatingStat.execute();
+            System.out.println("What is wrong with excuteing the query");
         }
         catch (SQLException e){
+            System.out.println("Just for the one");
+        }
+        try(Connection connectionNew = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);){
+            Iterator<String> iterator = foods.iterator();
+            System.out.println(foods);
+            while(iterator.hasNext()){
+                String sqlQueryNew = " ALTER TABLE " + tableName + " ADD COLUMN " + iterator.next() + " INT";
+                System.out.println(sqlQueryNew);
+                PreparedStatement creatingStatment = connectionNew.prepareStatement(sqlQueryNew);
+                creatingStatment.execute();
+                System.out.println("Tell me it works now");
+            }
+        }
+        catch (SQLException e){
+            System.out.println("Same here");
+        }
+    }
+    // IN the meantime lets write the JDBC for Attendance
+    public static void attendanceInfo(DefaultTableModel tableModel){
+        List<List<Object>> attendanceInfo = new ArrayList<>();
+
+        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+            System.out.println("I am here");
+            String sqlQuery = "SELECT breakfast.givenNumber, students.student_full_name, students.School_ID, students.Department, students.year," +
+                    " breakfast.entrytime, lunch.entrytime, dinner.entrytime " +
+                    " FROM breakfast " +
+                    " JOIN students ON breakfast.student_ID = students.student_ID " +
+                    " JOIN lunch ON breakfast.student_ID = lunch.student_ID " +
+                    " JOIN dinner ON breakfast.student_ID = dinner.student_ID;";
+            PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+            ResultSet result = creatingStat.executeQuery();
+            System.out.println("I have executed the query, now I have the data ");
+            result.next();
+            System.out.println(result.getString(1));
+            System.out.println(result.getString(2));
+            System.out.println(result.getString(3));
+            System.out.println(result.getString(4));
+            System.out.println(result.getString(5));
+            while(result.next()){
+                // You have to add it each row at a time
+                int studentID = result.getInt(1);
+                String studentName = result.getString(2);
+                String School_ID = result.getString(3);
+                String department = result.getString(4);
+                String year = result.getString(5);
+                Timestamp breakfastTime = result.getTimestamp(6);
+                Timestamp lunchTime = result.getTimestamp(7);
+                Timestamp dinnerTime = result.getTimestamp(8);
+                System.out.println("I am in the loop," + studentName +  studentID);
+                // Add the data to the table model
+                tableModel.addRow(new Object[]{studentName, School_ID, department, year, breakfastTime, lunchTime, dinnerTime});
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            System.out.println("Same here");
+        }
+    }
+    public static String getStudentID(String studentName){
+        String logInStudentID = null;
+        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+            System.out.println("I am before the query");
+            String sqlQuery = "SELECT student_ID FROM students WHERE student_full_name = ?";
+            PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+            creatingStat.setString(1,studentName);
+            System.out.println("If not it is before executing the query ");
+            ResultSet result = creatingStat.executeQuery();
+            System.out.println("It must be here");
+            if (result.next()) {
+                logInStudentID = result.getString("student_ID");
+                System.out.println(logInStudentID);
+            }
+            else
+                System.out.println("I don't have any data");
+        }
+        catch (SQLException e){
+            System.out.println("What is wrong with that");
+            e.printStackTrace();
+        }
+        return logInStudentID;
+    }
+
+    public static boolean rateChecker(){
+        boolean bool = false;
+        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+            String sqlQuery = "SELECT student_ID FROM rating WHERE student_ID = ? ";
+            PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+            creatingStat.setString(1,LoginPageForStudent.logInStudentID);
+            ResultSet result = creatingStat.executeQuery();
+            if (result.next()){
+                bool = true;
+            }
+        }
+        catch(SQLException e){
+            System.out.println("same here");
+            e.printStackTrace();
 
         }
-
+        return bool;
     }
-<<<<<<< HEAD
+    public static void  addRating(int rate){
+        if (rateChecker()){
+            try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+                String sqlQuery = "INSERT INTO rating (Number, Rates, student_ID) VALUES (?,?,?)";
+                PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+                creatingStat.setInt(1,0);
+                creatingStat.setInt(2,rate);
+                creatingStat.setString(3, LoginPageForStudent.logInStudentID);
+                Integer result = creatingStat.executeUpdate();
+                if (result != 0) {
+                    // Leykun show add some page to show you have rated successfully
+                }
+            }
+            catch (SQLException e){
+                System.out.println("Same here");
+                e.printStackTrace();
+            }
+        }
+        else{
+            //Leykun should create a page that will we displayed to show he has already rated the cafe
+        }
+    }
+    public static ArrayList<Integer> rateExtractor(){
+        ArrayList<Integer> rates = new ArrayList<>();
+        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+            String sqlQuery = "SELECT Rates FROM rating";
+            PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+            ResultSet result = creatingStat.executeQuery();
+            while(result.next()){
+                rates.add(result.getInt("Rates"));
+            }
+        }
+        catch(SQLException e){
+            System.out.println("Problem in rate extractor");
+            e.printStackTrace();
+        }
+        return rates;
+    }
+    // I need those Arguments.
+    public static boolean imageAdder(String imageName, String imagePath, String Day, String Meal){
+        boolean bool = false;
+        try(Connection connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+            String sqlQuery = "UPDATE Foods SET Number = ?, Food_Name = ?, Food_File_Path = ?, Days = ?, Meal = ? WHERE Days = ? AND Meals = ?";
+            PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+            creatingStat.setInt(1,0);
+            creatingStat.setString(2,imageName);
+            creatingStat.setString(3,imagePath);
+            creatingStat.setString(4,Day);
+            creatingStat.setString(5,Meal);
+            creatingStat.setString(6,Day);
+            creatingStat.setString(7,Meal);
 
-=======
->>>>>>> 90300935aed25c27243aeff9f964403719ffc15e
-     */
+            int result = creatingStat.executeUpdate();
+            if (result != 0){
+                bool = true;
+                // Leykun can use this to display something on the GUI
+                //This will call Leykun's Method with the specified Image name and Image File path
+            }
+        }
+        catch (SQLException e){
+            System.out.println("when adding an image");
+            e.printStackTrace();
+        }
+        return bool;
+    }
+
+    public static ArrayList<String> imageExtractor(String Day, String Meal){
+        ArrayList<String> imageInfo  = new ArrayList<>();
+        try(Connection connection  = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD)){
+            String sqlQuery = "SELECT Food_Name, Food_File_Path FROM Foods WHERE Days = ? AND Meals = ? ";
+            PreparedStatement creatingStat = connection.prepareStatement(sqlQuery);
+            creatingStat.setString(1,Day);
+            creatingStat.setString(2,Meal);
+            ResultSet result = creatingStat.executeQuery();
+            imageInfo.add(result.getString("Food_Name"));
+            imageInfo.add(result.getString("Food_File_Path"));
+        }
+        catch(SQLException e){
+            System.out.println("When Extracting Image");
+            e.printStackTrace();
+        }
+        return imageInfo;
+    }
 }
 
 
