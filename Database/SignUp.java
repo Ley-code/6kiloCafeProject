7b1d5279c57069938d31025c17da5b8e9f7f07fe
Amir-Fasil <amirfasil09@gmail.com>
2024-01-31 13:04:12 +0300

    finalized the poll database

--- Database/SignUp.java
+++ Database/SignUp.java
@@ -18,24 +18,27 @@ public class SignUp extends Backend{
     // This method will add our total information to database.
     // and return if we did register correctly.
     public static void arrayCreator() throws SQLException {
-        String[] studentInfo = new String[5];
+        String[] studentInfo = new String[6];
         // here you put the condition.
         studentInfo[0] = Backend.stuID;
         studentInfo[1] = Backend.studentName;
         studentInfo[2] = Backend.password;
         studentInfo[3] = Backend.Department;
         studentInfo[4] = Backend.year;
+        studentInfo[5] = Backend.school_ID;
+
         int result = ConnectionWithDatabase.signingUp(studentInfo);// what does resultSet will return if no row is affected
         System.out.println("row affected"+result);
         // Write your condition here
         //if true You will return You have signed up and load the next page
     }
     // This method would be called in th Action Listener of the SignUp button.
-    public static void infoInitialize(String studentName, String password, String Department, String year){
+    public static void infoInitialize(String studentName, String password, String Department, String year, String school_ID){
         Backend.studentName = studentName;
         Backend.password = password;
         Backend.Department = Department;
         Backend.year = year;
+        Backend.school_ID = school_ID;
 
     }
 }
