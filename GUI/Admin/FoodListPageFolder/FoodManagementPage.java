32df2feebd7361beb07a2ba1cde9063d7bb90298
Leykun Birhanu <leykunbi12@gmail.com>
2024-01-31 17:02:59 +0300

    finalized the foodmanagement page and integrated the attendance page table

--- GUI/Admin/FoodListPageFolder/FoodManagementPage.java
+++ GUI/Admin/FoodListPageFolder/FoodManagementPage.java
@@ -100,9 +100,9 @@ public class FoodManagementPage extends WelcomePage {
         previewButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //ArrayList<String> imagevalues = ConnectionWithDatabase.imageExtractor(Date,Mealtime);
-                //ImageLabel.setIcon(FoodIcon.scaledIcon(imagevalues.get(1),300,300));
-                //ImageLabel.setText(imagevalues.get(0));
+                ArrayList<String> imagevalues = ConnectionWithDatabase.imageExtractor(Date,Mealtime);
+                ImageLabel.setIcon(FoodIcon.scaledIcon(imagevalues.get(1),300,300));
+                ImageLabel.setText(imagevalues.get(0));
             }
         });
         //-------------------------------------------------------
